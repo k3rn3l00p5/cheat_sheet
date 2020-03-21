6494ac41e16ab3bf72124b2dd2c686754e3155a9
Kernel <nbelsito02@gmail.com>
2020-03-21 03:59:32 -0400

    project management

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-// removes dead code and unused variable warnings
+// removes dead code and unused variable/import warnings
 #![allow(unused_variables)]
 #![allow(dead_code)]
+#![allow(unused_imports)]
 
-// bring something into the scope with use
-use std::cmp::Ordering;
+// bring a path into the scope with use
 use std::io;
+// path name changing with "as"
+use crate::other_file::change_me as change;
+// re-exporting "use" with "pub"
+pub use std::cmp::Ordering;
+
+// load contents of a file with the same name
+mod other_file;
+// bring all public paths in with glob operator (*)
+use crate::other_file::*;
 
 const DATA3: u32 = 2_0; // const variable with visual separator (_)
 
---
+++ src/other_file.rs
@@ -0,0 +1,3 @@
+// modules in a separate file
+pub mod stuff {}
+pub mod change_me {}
